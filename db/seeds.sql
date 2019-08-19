21025d4aee72c4d22380aa4a7aab896efed5785d
Tyler Nichols <tegulator98@gmail.com>
2019-08-18 18:04:19 -0600

    DB set up and 3 burgers to start.

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO "burgers" (burger_name,devoured) VALUES ("Western Burger",false)
+
+INSERT INTO "burgers" (burger_name,devoured) VALUES ("Breakfast Burger",false)
+
+INSERT INTO "burgers" (burger_name,devoured) VALUES ("Big Boi",false)
