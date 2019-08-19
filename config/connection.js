15dad5fd39654a9a663db08959e901c96a101dc1
Tyler Nichols <tegulator98@gmail.com>
2019-08-19 00:43:04 -0600

    Bug fixes and final done.

--- config/connection.js
+++ config/connection.js
@@ -3,10 +3,6 @@ var mysql = require("mysql");
 
 var connection;
 
-if(process.env.burger_db){
- connection = mysql.createConnection(process.env.burger_db)
-}
-else{
 
 connection = mysql.createConnection({
   host: "localhost",
@@ -16,7 +12,7 @@ connection = mysql.createConnection({
   database: "burger_db"
 });
 
-}
+
 
 
 connection.connect(function(err) {
---
+++ public/js/burgers.js
@@ -0,0 +1,61 @@
+$(function() {
+    $(".change-devoure").on("click", function(event) {
+      var id = $(this).data("id");
+      var newDedevoure = $(this).data("newdevoure");
+  
+      var newDevouvreState = {
+        devoured: newDedevoure
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newDevouvreState
+      }).then(
+        function() {
+          console.log("changed devoure to", newDedevoure);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        burger_name: $("#bg").val().trim(),
+        devoured: 0
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("Added new Burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".delete-burger").on("click", function(event) {
+      var id = $(this).data("id");
+
+  
+  
+      // Send the DELETE request.
+      $.ajax("/api/burgers/" + id, {
+        type: "DELETE"
+      }).then(
+        function() {
+          console.log("deleted Burger", id);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
